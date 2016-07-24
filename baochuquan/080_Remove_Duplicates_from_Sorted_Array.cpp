8513a23612ab53f173be3a2bc2b76e57573167c5
baochuquan <baochuquan@163.com>
2016-07-24 19:29:33 +0800

    完成了第080题

---
+++ baochuquan/080_Remove_Duplicates_from_Sorted_Array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums){
+        int len = nums.size();
+        if(len <= 2)
+            return len;
+
+        int index = 2;
+        for(int i = 2; i < len; i++){
+            if(nums[i] != nums[index-2]){
+                nums[index++] = nums[i];
+            }
+        }
+        return index;
+    }
+};
