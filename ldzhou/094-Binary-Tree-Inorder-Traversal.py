c458b2d21982ef49a5594ad7404938e279a26005
周鲁东 <ludongzhou@gmail.com>
2016-08-02 23:16:38 +0800

    添加了中序遍历的非递归版本, 其实很简单

--- ldzhou/094-Binary-Tree-Inorder-Traversal.py
+++ ldzhou/094-Binary-Tree-Inorder-Traversal.py
@@ -14,3 +14,33 @@ class Solution(object):
         if not root:
             return []
         return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
+
+# 添加了非递归版本, 其实非常简单
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        stack = list()
+        cur = root
+        while cur:
+            stack.append(cur)
+            cur = cur.left
+        ans = list()
+        
+        while stack:
+            front = stack.pop()
+            ans.append(front.val)
+            cur = front.right
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+        return ans
