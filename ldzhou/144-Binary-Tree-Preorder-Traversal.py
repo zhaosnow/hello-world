ad48ecbf6df4c79126829bc1bdaebb59e8ae0f48
周鲁东 <ludongzhou@gmail.com>
2016-08-02 23:13:00 +0800

    添加了先序遍历的非递归版本, 非常简单

--- ldzhou/144-Binary-Tree-Preorder-Traversal.py
+++ ldzhou/144-Binary-Tree-Preorder-Traversal.py
@@ -14,3 +14,30 @@ class Solution(object):
         if not root:
             return []
         return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root): # 非递归版本, 其实很简单
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        stack = list()
+        if root:
+            stack.append(root)
+        ans = list()
+        
+        while stack:
+            top = stack.pop()
+            ans.append(top.val)
+            if top.right:
+                stack.append(top.right)
+            if top.left:
+                stack.append(top.left)
+        return ans
