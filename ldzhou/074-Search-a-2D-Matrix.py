2ebf6defc8d15261d27fc1c7ff82d3d7dc8382cf
周鲁东 <ludongzhou@gmail.com>
2016-08-04 23:54:11 +0800

    finish 074

---
+++ ldzhou/074-Search-a-2D-Matrix.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        cur_i, cur_j = len(matrix) - 1, 0
+        while 0 <= cur_i and cur_j < len(matrix[0]):
+            if matrix[cur_i][cur_j] == target:
+                return True
+            elif matrix[cur_i][cur_j] < target:
+                cur_j += 1
+            else:
+                cur_i -= 1
+        return False
